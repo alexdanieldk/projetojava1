6564cef350db8234b306f86f969bccfbc41a31e1
mrkha <mrkha@192.168.0.145>
2023-02-27 10:28:43 -0300

    Criando projeto

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -8,6 +8,7 @@ public class PrimeiraClasseJava {
 		/*Escrevendo texto no console*/
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
