9307bf7329dfc8f2c941fe4b4190a8c9344479c3
mrkha <mrkha@192.168.0.145>
2023-02-27 09:34:34 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		/*Escrevendo texto no console*/
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
